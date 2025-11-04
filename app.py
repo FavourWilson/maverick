6ac42c5fac65b8662c9f0050ca9848fd2c6a49d8
ebrusike favour <ebrusikefavour@gmail.com>
2025-11-04 15:27:23 +0100

    first push

---
+++ app.py
@@ -0,0 +1,96 @@
+import os
+from flask import Flask, jsonify, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import SelectField
+from brownie import accounts, Contract, network
+from dotenv import load_dotenv
+load_dotenv()
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "MAVERICKS"
+network.connect('arbitrum-sepolia')
+usdcAddress = Contract('0x1062BACf25bcC57C524cBA51D98d823B625f752f')
+defi_contract = Contract ('0x813549dD128F3a356FCBC67AeDcDDff9AD2992DF') 
+account = accounts.add(os.getenv("PRIVATE_KEY"))
+accounts.add(os.getenv("PRIVATE_KEY1"))
+accounts.add(os.getenv("PRIVATE_KEY2"))
+
+class Form(FlaskForm):
+    Faccounts = SelectField('Accounts', choices=['0xe17a6Bdd8041e37C4acE44db74811f00384F91E0',
+                                                 '0x9234E4349973F2B8eBF027E328ecd279Bf8786D1',
+                                                 '0x916B71993118CfBd3fFE3A4231631A43bd4C6a0f'])
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/deposit') 
+def deposit():
+    form = Form()
+    AvailableBal = SC_getAccountbal() / (10**18)
+    DepositedAmount = defi_contract.depositBalance(account)/ (10**18)
+    return render_template('deposit.html', form = form, AvailableBal = AvailableBal, DepositedAmount = DepositedAmount)
+
+
+
+@app.route('/depositButton', methods =['GET', 'POST'])
+def depositButton():
+    form = Form()
+    if request.method =='POST':
+      depositAmount = request.form.get("depositValue", type = int) * (10**18)
+      SC_depositBal(depositAmount)
+      DepositedAmount = defi_contract.depositBalance(account) /(10**18)
+      AvailableBal =  usdcAddress.balanceOf(account)/(10**18)
+    return render_template('deposit.html', form = form, AvailableBal = AvailableBal, DepositedAmount = DepositedAmount)
+
+@app.route('/withdrawButton', methods =['GET', 'POST'])
+def withdrawButton():
+    form = Form()
+    if request.method =='POST':
+      withdrawAmount = request.form.get("withdrawValue", type = int) * 10**18
+      SC_withdrawBal(withdrawAmount)
+      DepositedAmount = defi_contract.depositBalance(account) /(10**18)
+      AvailableBal =  usdcAddress.balanceOf(account)/(10**18)
+    return render_template('deposit.html', form = form, AvailableBal = AvailableBal, DepositedAmount = DepositedAmount)
+
+
+
+def SC_withdrawBal(withdrawAmount):
+     defi_contract.withdraw(withdrawAmount, {"from": account})
+
+def SC_getAccountbal():
+    balance = usdcAddress.balanceOf(account)
+    return balance 
+def SC_depositBal(depositAmount):
+    usdcAddress.approve(defi_contract, depositAmount, {"from": account})
+    defi_contract.depositToken(depositAmount, {"from": account})
+@app.route('/refresh/<currentAccount>')
+def refresh(currentAccount):
+    global account
+    account = accounts.at(currentAccount)
+    currentBal =  usdcAddress.balanceOf(account)/(10**18)
+    stakedBalance = defi_contract.depositBalance(account) /(10**18)
+    return jsonify({'response' :currentAccount , 'stakedBalance' : stakedBalance , 'currentBal' : currentBal})
+
+
+
+
+@app.route('/FundMe', methods =["GET", "POST"])
+def FundMe():
+    if request.method == "POST":
+        FromAddress = request.form.get("fromAddress")
+        FromAddress = accounts.at(FromAddress, force=True)
+        ToAddress = request.form.get("toAddress")
+        Amount = request.form.get("Amount", type = int)
+        usdcAddress.transfer(ToAddress, Amount * 10 ** 18, {"from": FromAddress})
+    return render_template('FundMe.html')
+
+
+
+
+if __name__ == "__main__":
+    
+    app.run()
+    network.disconnect()
